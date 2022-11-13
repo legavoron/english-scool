9ec4b2613d893e9ca6473461b326034f32c6e8e2
Aleh Shchurko <alehshchurko@gmail.com>
2022-11-13 17:59:26 +0300

    fixed to functional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 
-import { Component } from 'react';
-
 import Screensaver from './components/screensaver/screensaver';
 import StartPage from './components/startPage/startPage';
 import LevelsList from './components/levelsList/levelsList';
 import Task from './components/task/task';
 
 import {Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
 
 import error from './img/error.jpg';
 import question from './img/question.jpg';
@@ -29,75 +28,83 @@ import unit14 from './units/unit14';
 import unit15 from './units/unit15';
 import unit16 from './units/unit16';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+  
+  const unitsList = {unit1: unit1, unit2: unit2, unit3: unit3, unit4: unit4, unit5: unit5, unit6: unit6, unit7: unit7, unit8: unit8, unit9: unit9, unit10: unit10, unit11: unit11, unit12: unit12, unit13: unit13, unit14: unit14, unit15: unit15, unit16: unit16};
 
-    this.unitsList = {unit1: unit1, unit2: unit2, unit3: unit3, unit4: unit4, unit5: unit5, unit6: unit6, unit7: unit7, unit8: unit8, unit9: unit9, unit10: unit10, unit11: unit11, unit12: unit12, unit13: unit13, unit14: unit14, unit15: unit15, unit16: unit16}
+  const units = {unit0: false, unit1: false, unit2: false, unit3: false, unit4: false, unit5: false, unit6: false, unit7: false, unit8: false, unit9: false, unit10: false, unit11: false, unit12: false, unit13: false, unit14: false, unit15: false, unit16: false};
 
 
-    this.units = {unit0: false, unit1: false, unit2: false, unit3: false, unit4: false, unit5: false, unit6: false, unit7: false, unit8: false, unit9: false, unit10: false, unit11: false, unit12: false, unit13: false, unit14: false, unit15: false, unit16: false}
+  const [levels, setLevels] = useState(units);
+  const [btnValue, setBtnValue] = useState('');
+  const [task, setTask] = useState({
+    word: 'Вы не выбрали уровень',
+    translate: '',
+    url: error,
+    answerImage: ''
+  });
 
-    this.state = {
-      levels: this.units,
-      btnValue: '',
-
-      task: {
-        word: 'Вы не выбрали уровень!',
-        translate: '',
-        url: error,
-        answerImage: ''
-      }      
-    }
-  }
+  // const [currentTask, setCurrentTask] = useState('');
+  const [isActiveTask, setIsActiveTask] = useState(false);
+  const [isError, setIsError] = useState(true);
   
 
-  chooseLevels = (event) => {
+  const chooseLevels = (event) => {
 
     const target = event.target.id;
-    const unit = this.state.levels;
+    const unit = levels;
     
     if (event.target.id === 'unit0') {
-      let changedUnits = this.units;
+      let changedUnits = units;
 
-      if (this.state.levels.unit0 === false) {
+      if (levels.unit0 === false) {
         for (let unit in changedUnits) {
           changedUnits[unit] = true
         }
-        this.setState({levels: changedUnits})
+        setLevels(changedUnits);
       } else {
         for (let unit in changedUnits) {
           changedUnits[unit] = false
         }
-        this.setState({levels: changedUnits})
+        setLevels(changedUnits);
       }
     } else {
         unit[target]  = !unit[target];
-        this.setState({levels: unit})
+        setLevels(unit);
     }
+    setIsError(false);
   }
 
-  getRandomNum = (max, min=0) => {
+  const getRandomNum = (max, min=0) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  }
 
-  chooseTask = () => {
-    
-    const units = [];
-    const unitsListOfState = this.state.levels;
-    for (const unit in unitsListOfState) {
-      if (unitsListOfState[unit]) {
-        units.push(unit)
+  const chooseTask = () => {
+    if (isActiveTask === false ) {
+
+      if (isError) {
+        return
       }
-    }
 
-    let maxNumUnits = 0;
-    let numUnit = 0;
-    
-    if (units.length > 0) {
+      const units = [];
+      
+      // В units хранится список выбранных unit. Если уровни не выбраны - он пустой
+      for (const unit in levels) {
+        if (levels[unit]) {
+          units.push(unit);
+        }
+      }
 
+      // Если не выбраны уровни, то выходим из функции
+      if (units.length === 0) {
+        return
+      }
+
+      let maxNumUnits = 0;
+      let numUnit = 0;
+      
       maxNumUnits = units.length - 1;
       let unitName = '';
 
@@ -105,56 +112,78 @@ class App extends Component {
         unitName = units[maxNumUnits];
       } else {
         maxNumUnits = units.length - 1;
-        numUnit = this.getRandomNum(maxNumUnits, 1);
+        numUnit = getRandomNum(maxNumUnits, 1);
         unitName = units[numUnit];
       }
       
-      const unit = this.unitsList[unitName];
+      const unit = unitsList[unitName];
 
       const maxNumTask = unit.length - 1;
-      const numTask = this.getRandomNum(maxNumTask);
-      const task = unit[numTask];
+      const numTask = getRandomNum(maxNumTask);
 
-      if (this.state.btnValue === '' || this.state.btnValue === 'Далее') {
-        const newStateTask = {
-          word: task.word,
-          translate: task.translate,
-          url: question,
-          answerImage: task.url
-        }
-  
-        this.setState({task: newStateTask});
-        this.setState({btnValue: 'Ответ'});
-      } else {
-        
-        const newStateTask = {
-          word: this.state.task.translate,
-          url: this.state.task.answerImage
-        }
-  
-        this.setState({task: newStateTask});
-        this.setState({btnValue: 'Далее'});
-      }
-    }
-  }
-  
-  exitFromTask = (event) => {
-    this.chooseTask(event)
-  }
+      // В currentTask хранится текущий task
+      console.log(unit[numTask]);
+      const currentTask = unit[numTask];
+      console.log(currentTask);
 
-  render() {
-    return (
-      <div className="App">
-        <Routes>
-            <Route exact path='/' element={<Screensaver/>}/>
-            <Route exact path='/main' element={<StartPage chooseTask={this.chooseTask}/>}/>
-            <Route exact path='/main/levels' element={<LevelsList chooseLevels={this.chooseLevels} colors={this.state.levels} chooseTask={this.chooseTask} />}/>
-            <Route exact path='/main/task' element={<Task units={this.state.levels} word={this.state.task.word}  url={this.state.task.url} btnValue={this.state.btnValue} chooseTask={this.chooseTask} exitFromTask={this.exitFromTask}/>} />
-        </Routes>
+      setTask({
+        word: currentTask.word,
+        translate: currentTask.translate,
+        url: question,
+        answerImage: currentTask.url
+      });
+      setBtnValue('Ответ');
+      setIsActiveTask(true);
+
+      console.log('isActiveTask: ' + isActiveTask);
+      console.log(task);
+
+
+    } else {
       
-    </div>
-    )
+      console.log('isActiveTask: ' + isActiveTask);
+      console.log(task);
+      const temporaryState = task;
+      console.log(temporaryState.url)
+
+      setTask({
+        word: temporaryState.translate,
+        translate: temporaryState.word,
+        url: temporaryState.answerImage
+      });
+
+      setBtnValue('Далее');
+      setIsActiveTask(false);
+      }
   }
+  
+
+  
+  const exitFromTask = (event) => {
+    chooseTask(event);
+  }
+
+
+  return (
+    <div className="App">
+      <Routes>
+          <Route exact path='/' element={<Screensaver/>}/>
+          <Route exact path='/main' element={<StartPage chooseTask={chooseTask}/>}/>
+          <Route exact path='/main/levels' element={<LevelsList 
+                                                      chooseLevels={chooseLevels} 
+                                                      colors={levels} 
+                                                      chooseTask={chooseTask} />}/>
+          <Route exact path='/main/task' element={<Task 
+                                                    units={levels} 
+                                                    word={task.word}  
+                                                    url={task.url} 
+                                                    btnValue={btnValue} 
+                                                    chooseTask={chooseTask} 
+                                                    exitFromTask={exitFromTask}/>} />
+      </Routes>
+    
+  </div>
+  )
 }
 
 export default App;
--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,34 +1,25 @@
 import './task.css';
 import {NavLink} from 'react-router-dom';
-import { Component } from 'react';
 
-class Task extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="task">
-                <NavLink to="/main">
-                    <div className="close">
-                    
-                    </div>
-                </NavLink>
-                <div className='taskImageContainer'>
-                    <img src={this.props.url} alt="question image" className='taskImage'/>
+const Task = (props) => {
+    return (
+        <div className="task">
+            <NavLink to="/main">
+                <div className="close">
+                
                 </div>
-                <div className="valueBlock">
-                    <h2 className="taskHeading">
-                            {this.props.word}
-                    </h2>
-                    <button className='taskBtn' onClick={this.props.chooseTask}>{this.props.btnValue}</button>
-                </div>
+            </NavLink>
+            <div className='taskImageContainer'>
+                <img src={props.url} alt="question image" className='taskImage'/>
             </div>
-        )
-    }
-
-    
+            <div className="valueBlock">
+                <h2 className="taskHeading">
+                        {props.word}
+                </h2>
+                <button className='taskBtn' onClick={props.chooseTask}>{props.btnValue}</button>
+            </div>
+        </div>
+    )
 }
 
 export default Task;
