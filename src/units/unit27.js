b01df47c2721cdb5d78e9bda7e7e9a450b7b2980
legavoron <alehshchurko@gmail.com>
2023-07-24 13:41:32 +0300

    add new words

--- src/units/unit27.js
+++ src/units/unit27.js
@@ -3,6 +3,38 @@ import collect from '../img/collect.jpg';
 import comparison from '../img/comparison.jpg';
 import giveThought from '../img/giveThought.jpg';
 import inManyWays from '../img/inManyWays.jpg';
+import estateAgent from '../img/estateAgent.jpg';
+import endless from '../img/endless.jpg';
+import choir from '../img/choir.jpg';
+import localPeople from '../img/localPeople.jpg';
+import spoil from '../img/spoil.jpg';
+import lyrics from '../img/lyrics.jpg';
+import wet from '../img/wet.jpg';
+import railwayStation from '../img/railwayStation.jpg';
+import electricity from '../img/electricity.jpg';
+import avoid from '../img/avoid.jpg';
+import citizen from '../img/citizen.jpg';
+import seniorCitizen from '../img/seniorCitizen.jpg';
+import cyclePath from '../img/cyclePath.jpg';
+import thirsty from '../img/thirsty.jpg';
+import shareholders from '../img/shareholders.jpg';
+import former from '../img/former.jpg';
+import assign from '../img/assign.jpg';
+import change from '../img/change.jpg';
+import refund from '../img/refund.jpg';
+import charge from '../img/charge.jpg';
+import thatsAnnoing from '../img/thatsAnnoing.jpg';
+import frustrating from '../img/frustrating.jpg';
+import boo from '../img/boo.jpg';
+import entrance from '../img/entrance.jpg';
+import knot from '../img/knot.jpg';
+import bookcase from '../img/bookcase.jpg';
+import sign from '../img/sign.jpg';
+import sights from '../img/sights.jpg';
+import unemployment from '../img/unemployment.jpg';
+import income from '../img/income.jpg';
+import beautifulScenery from '../img/beautifulScenery.jpg';
+import toGetAway from '../img/toGetAway.jpg';
 
 
 
@@ -33,11 +65,170 @@ const unit26 = [
         translate: "in many ways", 
         url: inManyWays
     },
+    {
+        word: 'Агент по недвижимости',
+        translate: "Estate agent", 
+        url: estateAgent
+    },
+    {
+        word: 'Бесконечный, нескончаемый',
+        translate: "endless", 
+        url: endless
+    },
+    {
+        word: 'Хор',
+        translate: "Choir", 
+        url: choir
+    },
+    {
+        word: 'Местное население, местные жители',
+        translate: "local people", 
+        url: localPeople
+    },
+    {
+        word: 'Портить, испортить',
+        translate: "spoil", 
+        url: spoil
+    },
+    {
+        word: 'Текст песни',
+        translate: "Lyrics", 
+        url: lyrics
+    },
+    {
+        word: 'Мокрый, влажный',
+        translate: "wet", 
+        url: wet
+    },
+    {
+        word: 'Железнодорожный вокзал',
+        translate: "railway station", 
+        url: railwayStation
+    },
+    {
+        word: 'Электричество, электроэнергия',
+        translate: "Electricity", 
+        url: electricity
+    },
+    {
+        word: 'Избегать (чего-то, кого-то)',
+        translate: "avoid", 
+        url: avoid
+    },
+    {
+        word: 'Гражданин',
+        translate: "Citizen", 
+        url: citizen
+    },
+    {
+        word: 'Пенсионер, пожилой человек',
+        translate: "senior citizen", 
+        url: seniorCitizen
+    },
+    {
+        word: 'Велодорожка',
+        translate: "scycle path", 
+        url: cyclePath
+    },
+    {
+        word: 'Жаждующий чего-либо',
+        translate: "thirsty for", 
+        url: thirsty
+    },
+    {
+        word: 'Акционеры',
+        translate: "Shareholders", 
+        url: shareholders
+    },
+    {
+        word: 'Предыдущий, прежний',
+        translate: "former", 
+        url: former
+    },
+    {
+        word: 'Назначить, поручить (задания)',
+        translate: "assign", 
+        url: assign
+    },
+    {
+        word: 'Сдача',
+        translate: "Сhange", 
+        url: change
+    },
+    {
+        word: 'Возмещение, компенсация',
+        translate: "Refund", 
+        url: refund
+    },
+    {
+        word: 'Взымать (налоги, плату)',
+        translate: "charge", 
+        url: charge
+    },
+    {
+        word: 'Это возмутительно',
+        translate: `that's annoing`, 
+        url: thatsAnnoing
+    },
+    {
+        word: 'Неприятно (разочаровывающий)',
+        translate: `frustrating`, 
+        url: frustrating
+    },
+    {
+        word: 'Свистеть, оствистывать',
+        translate: `boo`, 
+        url: boo
+    },
+    {
+        word: 'Вход, въезд',
+        translate: `Entrance`, 
+        url: entrance
+    },
+    {
+        word: 'Узел',
+        translate: `Knot`, 
+        url: knot
+    },
+    {
+        word: 'Книжный шкаф',
+        translate: `Bookcase`, 
+        url: bookcase
+    },
+    {
+        word: 'Вывеска, знак',
+        translate: `Sign`, 
+        url: sign
+    },
+    {
+        word: 'Достопримечательность',
+        translate: `Sights`, 
+        url: sights
+    },
+    {
+        word: 'Безработица',
+        translate: `Unemployment`, 
+        url: unemployment
+    },
+    {
+        word: 'Доход, прибыль',
+        translate: `Income`, 
+        url: income
+    },
+    {
+        word: 'Красивый вид (красивый пейзаж)',
+        translate: `beautiful scenery`, 
+        url: beautifulScenery
+    },
+    {
+        word: 'Уйти подальше ото всех',
+        translate: `to get away from it all`, 
+        url: toGetAway
+    },
 
 
 ]
 
 export default unit26;
-
 
 
