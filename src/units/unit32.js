5210079270c05062cad8cc79ab6576abc7d8db57
legavoron <alehshchurko@gmail.com>
2023-10-19 20:23:35 +0300

    add new words

--- src/units/unit32.js
+++ src/units/unit32.js
@@ -1,22 +1,157 @@
-import maintenance from '../img/maintenance.jpg';
+import impression from '../img/impression.jpg';
+import basement from '../img/basement.jpg';
+import Insufficient from '../img/Insufficient.jpg';
+import restMyEyes from '../img/restMyEyes.jpg';
+import beMuddledUp from '../img/beMuddledUp.jpg';
+import appreciate from '../img/appreciate.jpg';
+import conclusion from '../img/conclusion.jpg';
+import intentions from '../img/intentions.jpg';
+import completely from '../img/completely.jpg';
+import ordinary from '../img/ordinary.jpg';
+import clearly from '../img/clearly.jpg';
+import crowd from '../img/crowd.jpg';
+import neck from '../img/neck.jpg';
+import brave from '../img/brave.jpg';
+import knight from '../img/knight.jpg';
+import forgive from '../img/forgive.jpg';
+import waterfall from '../img/waterfall.jpg';
+import proudly from '../img/proudly.jpg';
+import keepMind from '../img/keepMind.jpg';
+import lickOf from '../img/lickOf.jpg';
+import pedestrian from '../img/pedestrian.jpg';
+import fame from '../img/fame.jpg';
+import along from '../img/along.jpg';
+import tongue from '../img/tongue.jpg';
 
 
 
 const unit32 = [
     {
-        word: 'Обслуживание, техническое обслуживание',
-        translate: `maintenance`, 
-        url: maintenance
+        word: 'Впечатление, ощущение',
+        translate: `Impression`, 
+        url: impression
     },
-
-
-
+    {
+        word: 'Подвал, цокольный этаж',
+        translate: `Basement`, 
+        url: basement
+    },
+    {
+        word: 'Недостаточно, недостаточный',
+        translate: `insufficient`, 
+        url: Insufficient
+    },
+    {
+        word: 'Отдохнуть глазам, закрыть глаза',
+        translate: `restMyEyes`, 
+        url: restMyEyes
+    },
+    {
+        word: 'Запутанный',
+        translate: `to be muddled up`, 
+        url: beMuddledUp
+    },
+    {
+        word: 'Ценить, благодарить',
+        translate: `appreciate`, 
+        url: appreciate
+    },
+    {
+        word: 'Заключение, вывод',
+        translate: `Conclusion`, 
+        url: conclusion
+    },
+    {
+        word: 'Намерения, цели, планы',
+        translate: `Intentions`, 
+        url: intentions
+    },
+    {
+        word: 'Совершенно, абсолютно',
+        translate: `completely`, 
+        url: completely
+    },
+    {
+        word: 'Обычный, обыденный',
+        translate: `ordinary`, 
+        url: ordinary
+    },
+    {
+        word: 'Явно, несомненно',
+        translate: `clearly`, 
+        url: clearly
+    },
+    {
+        word: 'Народ, толпа',
+        translate: `Crowd`, 
+        url: crowd
+    },
+    {
+        word: 'Шея',
+        translate: `Neck`, 
+        url: neck
+    },
+    {
+        word: 'Храбрый, смелый',
+        translate: `brave`, 
+        url: brave
+    },
+    {
+        word: 'Рыцарь',
+        translate: `Knight`, 
+        url: knight
+    },
+    {
+        word: 'Прощать, простить',
+        translate: `forgive`, 
+        url: forgive
+    },
+    {
+        word: 'Водопад',
+        translate: `Waterfall`, 
+        url: waterfall
+    },
+    {
+        word: 'Гордо',
+        translate: `proudly`, 
+        url: proudly
+    },
+    {
+        word: 'Имей в виду',
+        translate: `keep mind`, 
+        url: keepMind
+    },
+    {
+        word: 'Чуть чуть, немного чего-то (нехватка)',
+        translate: `lick of`, 
+        url: lickOf
+    },
+    {
+        word: 'Пешеход',
+        translate: `Pedestrian`, 
+        url: pedestrian
+    },
+    {
+        word: 'Популярность, слава',
+        translate: `Fame`, 
+        url: fame
+    },
+    {
+        word: 'Вдоль чего-то',
+        translate: `along`, 
+        url: along
+    },
+    {
+        word: 'Вдоль чего-то',
+        translate: `Tongue`, 
+        url: tongue
+    },
+   
     
 
 
 ]
 
 export default unit32;
-
 
 
