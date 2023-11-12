41697aae2b2f552a150156b0571ab276a5b035fb
legavoron <alehshchurko@gmail.com>
2023-11-12 18:06:11 +0300

    add new words to unit1

--- src/units/unit1.js
+++ src/units/unit1.js
@@ -1,32 +1,33 @@
-import good from '../img/good.jpg';
 import appropriate from '../img/appropriate.jpg';
-import courageous from '../img/courageous.jpg';
-import cheerful from '../img/cheerful.jpg';
 import influence from '../img/influence.jpg';
 import persist from '../img/persist.jpg';
 import produce from '../img/produce.jpg';
+import commuting from '../img/commuting.jpg';
+import spareTime from '../img/spareTime.jpg';
+import daydream from '../img/daydream.jpg';
+import commuters from '../img/commuters.jpg';
+import overcrowding from '../img/overcrowding.jpg';
+import bootUp from '../img/bootUp.jpg';
+import vehicles from '../img/vehicles.jpg';
+import thrill from '../img/thrill.jpg';
+import roughly from '../img/roughly.jpg';
+import mention from '../img/mention.jpg';
+import beWilling from '../img/beWilling.jpg';
+import shy from '../img/shy.jpg';
+import warn from '../img/warn.jpg';
+import propose from '../img/propose.jpg';
+import launch from '../img/launch.jpg';
+import contractor from '../img/contractor.jpg';
+import beResponsible from '../img/beResponsible.jpg';
+
+
 
 
 const unit1 = [
     {
-      word: 'За исключением',
-      translate: 'except', 
-      url: good
-    },
-    {
       word: 'Подходит (соответствует)',
       translate: 'appropriate', 
       url: appropriate
-    },
-    {
-      word: 'Смелый',
-      translate: 'courageous', 
-      url: courageous
-    },
-    {
-      word: 'Весёлый',
-      translate: 'cheerful', 
-      url: cheerful
     },
     {
       word: 'Влияние, авторитет',
@@ -42,6 +43,91 @@ const unit1 = [
       word: 'Производить',
       translate: 'produce', 
       url: produce
+    },
+    {
+      word: 'Время в дороге',
+      translate: 'commuting', 
+      url: commuting
+    },
+    {
+      word: 'Свободное время',
+      translate: 'spare tme', 
+      url: spareTime
+    },
+    {
+      word: 'Мечтать, фантазировать',
+      translate: 'daydream', 
+      url: daydream
+    },
+    {
+      word: 'Те, кто едут на работу',
+      translate: 'Commuters', 
+      url: commuters
+    },
+    {
+      word: 'Перенаполнение чего-то',
+      translate: 'Overcrowding', 
+      url: overcrowding
+    },
+    {
+      word: 'Загрузка, включение устройства (компьютера)',
+      translate: 'boot up', 
+      url: bootUp
+    },
+    {
+      word: 'Транспортное средство',
+      translate: 'vehicles', 
+      url: vehicles
+    },
+    {
+      word: 'Чувство азарта, волнение, возбуждение',
+      translate: 'Thrill', 
+      url: thrill
+    },
+    {
+      word: 'Примерно, около, приблизительно',
+      translate: 'roughly', 
+      url: roughly
+    },
+    {
+      word: 'Упоминуть, назвать',
+      translate: 'mention', 
+      url: mention
+    },
+    {
+      word: 'Готов, охотно согласен',
+      translate: 'to be willing to', 
+      url: beWilling
+    },
+    {
+      word: 'Застенчевый, стеснительный',
+      translate: 'shy', 
+      url: shy
+    },
+    {
+      word: 'Предупредить о чем-то',
+      translate: 'warn', 
+      url: warn
+    },
+    {
+      word: 'Предложить что-то',
+      translate: 'propose', 
+      url: propose
+    },
+    {
+      word: 'Запуск, старт',
+      translate: 'launch', 
+      url: launch
+    },
+    {
+      word: 'Подрядчик',
+      translate: 'contractor', 
+      url: contractor
+    },
+    {
+      word: 'Быть ответственным за',
+      translate: 'to be responsible for', 
+      url: beResponsible
     },
   ]
 
