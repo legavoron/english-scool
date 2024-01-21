e871fb7ef01089783d0cd4a05d78259240e00209
legavoron <alehshchurko@gmail.com>
2024-01-21 17:08:02 +0300

    add new words

--- src/units/unit4.js
+++ src/units/unit4.js
@@ -28,6 +28,38 @@ import commonSense from '../img/commonSense.jpg';
 import thinkOnMyFeet from '../img/thinkOnMyFeet.jpg';
 import asses from '../img/asses.jpg';
 import carryOn from '../img/carryOn.jpg';
+import enrolment from '../img/enrolment.jpg';
+import beDistracted from '../img/beDistracted.jpg';
+import intimidating from '../img/intimidating.jpg';
+import tutor from '../img/tutor.jpg';
+import affordable from '../img/affordable.jpg';
+import resist from '../img/resist.jpg';
+import raiseObjections from '../img/raiseObjections.jpg';
+import goAlongWith from '../img/goAlongWith.jpg';
+import takeRisks from '../img/takeRisks.jpg';
+import viceVersa from '../img/viceVersa.jpg';
+import planFOrFuture from '../img/planFOrFuture.jpg';
+import improvise from '../img/improvise.jpg';
+import adaptTo from '../img/adaptTo.jpg';
+import surprisingly from '../img/surprisingly.jpg';
+import risky from '../img/risky.jpg';
+import script from '../img/script.jpg';
+import rehearsal from '../img/rehearsal.jpg';
+import makeThingsUp from '../img/makeThingsUp.jpg';
+import stickTo from '../img/stickTo.jpg';
+import assertiveness from '../img/assertiveness.jpg';
+import consideration from '../img/consideration.jpg';
+import preference from '../img/preference.jpg';
+import awareness from '../img/awareness.jpg';
+import purchase from '../img/purchase.jpg';
+import impact from '../img/impact.jpg';
+import advocacy from '../img/advocacy.jpg';
+import mediation from '../img/mediation.jpg';
+import persuasion from '../img/persuasion.jpg';
+import approach from '../img/approach.jpg';
+import introduction from '../img/introduction.jpg';
+import custom from '../img/custom.jpg';
+import requirements from '../img/requirements.jpg';
 
 
 
@@ -109,7 +141,7 @@ const unit4 = [
       url: persuasive
     },
     {
-      word: 'Матемотически грамотный',
+      word: 'Математически грамотный',
       translate: `numerate`, 
       url: numerate
     },
@@ -183,7 +215,166 @@ const unit4 = [
       translate: `carryOn`, 
       url: carryOn
     },
-
+    {
+      word: 'Зачисление, прием, регистрация',
+      translate: `enrolment`, 
+      url: enrolment
+    },
+    {
+      word: 'Отвчлечься, отвлеченный',
+      translate: `to be disrtacted`, 
+      url: beDistracted
+    },
+    {
+      word: 'Пугающий, устрашающий',
+      translate: `intimidating`, 
+      url: intimidating
+    },
+    {
+      word: 'Репетитор, преподаватель',
+      translate: `Tutor`, 
+      url: tutor
+    },
+    {
+      word: 'Доступный по цене, недорогой',
+      translate: `affordable`, 
+      url: affordable
+    },
+    {
+      word: 'Сопротивляться, противится',
+      translate: `resist`, 
+      url: resist
+    },
+    {
+      word: 'Высказать возражения',
+      translate: `raise objections`, 
+      url: raiseObjections
+    },
+    {
+      word: 'Согласить с кем-то',
+      translate: `go along with...`, 
+      url: goAlongWith
+    },
+    {
+      word: 'Рисковать',
+      translate: `take risks`, 
+      url: takeRisks
+    },
+    {
+      word: 'Наоборот',
+      translate: `vice versa`, 
+      url: viceVersa
+    },
+    {
+      word: 'Планировать будущее',
+      translate: `plan for future`, 
+      url: planFOrFuture
+    },
+    {
+      word: 'Импровизировать',
+      translate: `improvise`, 
+      url: improvise
+    },
+    {
+      word: 'Адаптироваться к ...',
+      translate: `adapt to`, 
+      url: adaptTo
+    },
+    {
+      word: 'Удивительно, что ...',
+      translate: `surprisingly`, 
+      url: surprisingly
+    },
+    {
+      word: 'Рискованный',
+      translate: `risky`, 
+      url: risky
+    },
+    {
+      word: 'Сценарий',
+      translate: `Script`, 
+      url: script
+    },
+    {
+      word: 'Репетиция',
+      translate: `Rehearsal`, 
+      url: rehearsal
+    },
+    {
+      word: 'Выдумывать на ходу',
+      translate: `make things up on the spot`, 
+      url: makeThingsUp
+    },
+    {
+      word: 'Придерживаться чего-то, продолжать делать что-то свое',
+      translate: `stick to`, 
+      url: stickTo
+    },
+    {
+      word: 'Уверенность в себе, решительность, напористость',
+      translate: `Assertiveness`, 
+      url: assertiveness
+    },
+    {
+      word: 'Принять во внимание',
+      translate: `take into consideration`, 
+      url: consideration
+    },
+    {
+      word: 'Предпочтение',
+      translate: `Preference`, 
+      url: preference
+    },
+    {
+      word: 'Осведомленность, информированность, понимание чего-то',
+      translate: `Awareness`, 
+      url: awareness
+    },
+    {
+      word: 'Покупка, приобретение чего-то',
+      translate: `Purchase`, 
+      url: purchase
+    },
+    {
+      word: 'Влияние, воздействие, последствия',
+      translate: `Impact`, 
+      url: impact
+    },
+    {
+      word: 'Защита интересов, пропаганда, адвокатское дело',
+      translate: `Advocacy`, 
+      url: advocacy
+    },
+    {
+      word: 'Посредничество',
+      translate: `Mediation`, 
+      url: mediation
+    },
+    {
+      word: 'Убеждение, уговор кого-то',
+      translate: `Persuasion`, 
+      url: persuasion
+    },
+    {
+      word: 'Подход, метод',
+      translate: `Approach`, 
+      url: approach
+    },
+    {
+      word: 'Введение, внедрение',
+      translate: `Introduction`, 
+      url: introduction
+    },
+    {
+      word: 'Обычай, традиция (индивидуальный)',
+      translate: `Custom`, 
+      url: custom
+    },
+    {
+      word: 'Требования, потребности',
+      translate: `Requirements`, 
+      url: requirements
+    },
 
 
 
