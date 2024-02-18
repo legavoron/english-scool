847756bc8e1eb86927b2df72d795d1855d86b6cb
legavoron <alehshchurko@gmail.com>
2024-02-18 17:05:32 +0300

    add new words, start unit 6

--- src/units/unit5.js
+++ src/units/unit5.js
@@ -16,6 +16,64 @@ import orphanage from '../img/orphanage.jpg';
 import camaraderie from '../img/camaraderie.jpg';
 import flock from '../img/flock.jpg';
 import lonely from '../img/lonely.jpg';
+import flood from '../img/flood.jpg';
+import boiling from '../img/boiling.jpg';
+import changeable from '../img/changeable.jpg';
+import flu from '../img/flu.jpg';
+import casualWear from '../img/casualWear.jpg';
+import plot from '../img/plot.jpg';
+import mayor from '../img/mayor.jpg';
+import localCouncil from '../img/localCouncil.jpg';
+import author from '../img/author.jpg';
+import elections from '../img/elections.jpg';
+import campsite from '../img/campsite.jpg';
+import government from '../img/government.jpg';
+import flipFlops from '../img/flipFlops.jpg';
+import vote from '../img/vote.jpg';
+import trafficJam from '../img/trafficJam.jpg';
+import freezing from '../img/freezing.jpg';
+import shower from '../img/shower.jpg';
+import gassipAbout from '../img/gassipAbout.jpg';
+import mutual from '../img/mutual.jpg';
+import urbeat from '../img/urbeat.jpg';
+import gesture from '../img/gesture.jpg';
+import relative from '../img/relative.jpg';
+import accompaniedBy from '../img/accompaniedBy.jpg';
+import part from '../img/part.jpg';
+import renovate from '../img/renovate.jpg';
+import housing from '../img/housing.jpg';
+import annual from '../img/annual.jpg';
+import enterprise from '../img/enterprise.jpg';
+import entrepreneur from '../img/entrepreneur.jpg';
+import publicity from '../img/publicity.jpg';
+import aspiring from '../img/aspiring.jpg';
+import lowIncome from '../img/lowIncome.jpg';
+import apprentice from '../img/apprentice.jpg';
+import aim from '../img/aim.jpg';
+import whatAreWeSupposedToDo from '../img/whatAreWeSupposedToDo.jpg';
+import goddamMiracle from '../img/goddamMiracle.jpg';
+import passedOn from '../img/passedOn.jpg';
+import wipesClean from '../img/wipesClean.jpg';
+import gloat from '../img/gloat.jpg';
+import withdrawalSymptoms from '../img/withdrawalSymptoms.jpg';
+import exhibit from '../img/exhibit.jpg';
+import domain from '../img/domain.jpg';
+import carefree from '../img/carefree.jpg';
+import orphaned from '../img/orphaned.jpg';
+import astounding from '../img/astounding.jpg';
+import custodian from '../img/custodian.jpg';
+import chaperone from '../img/chaperone.jpg';
+import jumpy from '../img/jumpy.jpg';
+import grief from '../img/grief.jpg';
+import spite from '../img/spite.jpg';
+import malice from '../img/malice.jpg';
+import surrender from '../img/surrender.jpg';
+import renounce from '../img/renounce.jpg';
+import decree from '../img/decree.jpg';
+import divorce from '../img/divorce.jpg';
+import archery from '../img/archery.jpg';
+import poison from '../img/poison.jpg';
+
 
 
 
@@ -111,7 +169,296 @@ const unit5 = [
       translate: 'lonely', 
       url: lonely
     },
-    
+    {
+      word: 'Потом, наводнение',
+      translate: 'Flood', 
+      url: flood
+    },
+    {
+      word: 'Кипячение, варка',
+      translate: 'boiling', 
+      url: boiling
+    },
+    {
+      word: 'Изменчивый, переменный',
+      translate: 'changeable', 
+      url: changeable
+    },
+    {
+      word: 'Грипп, ОРВИ',
+      translate: 'Flu', 
+      url: flu
+    },
+    {
+      word: 'Повседневная одежда',
+      translate: 'casual wear', 
+      url: casualWear
+    },
+    {
+      word: 'Сюжет',
+      translate: 'Plot', 
+      url: plot
+    },
+    {
+      word: 'Мэр',
+      translate: 'Mayor', 
+      url: mayor
+    },
+    {
+      word: 'Местная власть',
+      translate: 'local council', 
+      url: localCouncil
+    },
+    {
+      word: 'Автор, писатель',
+      translate: 'Author', 
+      url: author
+    },
+    {
+      word: 'Выборы',
+      translate: 'Elections', 
+      url: elections
+    },
+    {
+      word: 'Кемпинг, палаточный лагерь',
+      translate: 'Campsite', 
+      url: campsite
+    },
+    {
+      word: 'Правительство, государство',
+      translate: 'Government', 
+      url: government
+    },
+    {
+      word: 'Шлепанцы. шлепки',
+      translate: 'Flip-flops', 
+      url: flipFlops
+    },
+    {
+      word: 'Голосование, выборы, голосовать',
+      translate: 'vote', 
+      url: vote
+    },
+    {
+      word: 'Пробка, затор',
+      translate: 'traffic jam', 
+      url: trafficJam
+    },
+    {
+      word: 'Мороз, холод',
+      translate: 'Freezing', 
+      url: freezing
+    },
+    {
+      word: 'Дождь, ливень, град',
+      translate: 'Shower', 
+      url: shower
+    },
+    {
+      word: 'Сплетничать о...',
+      translate: 'gassip about', 
+      url: gassipAbout
+    },
+    {
+      word: 'Взаимный',
+      translate: 'mutual', 
+      url: mutual
+    },
+    {
+      word: 'Оптимистичный, жизнерадостный',
+      translate: 'upbeat', 
+      url: urbeat
+    },
+    {
+      word: 'Жест, телодвижение',
+      translate: 'Gesture', 
+      url: gesture
+    },
+    {
+      word: 'Родственники',
+      translate: 'Relatives', 
+      url: relative
+    },
+    {
+      word: 'Сопровождается чем-то',
+      translate: 'accompanied by', 
+      url: accompaniedBy
+    },
+    {
+      word: 'Прощаться, расстоваться',
+      translate: 'part', 
+      url: part
+    },
+    {
+      word: 'Обновить, отремонтировать',
+      translate: 'renovate', 
+      url: renovate
+    },
+    {
+      word: 'Жилье, жилищные условия',
+      translate: 'Housing', 
+      url: housing
+    },
+    {
+      word: 'Ежегодный',
+      translate: 'annual', 
+      url: annual
+    },
+    {
+      word: 'Доход, выручка',
+      translate: 'Revenue', 
+      url: annual
+    },
+    {
+      word: 'Предприятие, компания',
+      translate: 'Enterprise', 
+      url: enterprise
+    },
+    {
+      word: 'Предприниматель',
+      translate: 'Entrepreneur', 
+      url: entrepreneur
+    },
+    {
+      word: 'Публичность, огласка, шумиха',
+      translate: 'Publicity', 
+      url: publicity
+    },
+    {
+      word: 'Начинающий, подающий надежды',
+      translate: 'aspiring', 
+      url: aspiring
+    },
+    {
+      word: 'Низкий доход',
+      translate: 'low income', 
+      url: lowIncome
+    },
+    {
+      word: 'Ученик, подмастерье',
+      translate: 'Apprentice', 
+      url: apprentice
+    },
+    {
+      word: 'Стремиться к..., задача, цель',
+      translate: 'aim', 
+      url: aim
+    },
+    {
+      word: 'Что нам с ней делать дальше?',
+      translate: 'What are we supposed to do with her?', 
+      url: whatAreWeSupposedToDo
+    },
+    {
+      word: 'Чертово чудо',
+      translate: 'goddamn miracle', 
+      url: goddamMiracle
+    },
+    {
+      word: 'Мертв, без сознания',
+      translate: 'passed on', 
+      url: passedOn
+    },
+    {
+      word: 'Очищается,стирается, чистится',
+      translate: 'wipes clean', 
+      url: wipesClean
+    },
+    {
+      word: 'Злорадствовать',
+      translate: `gloat`, 
+      url: gloat
+    },
+    {
+      word: 'Симптомы отмены',
+      translate: `withdrawal symptoms`, 
+      url: withdrawalSymptoms
+    },
+    {
+      word: 'Выставка',
+      translate: `Exhibit`, 
+      url: exhibit
+    },
+    {
+      word: 'Область, сфера',
+      translate: `Domain`, 
+      url: domain
+    },
+    {
+      word: 'Беззаботный',
+      translate: `carefree`, 
+      url: carefree
+    },
+    {
+      word: 'Сирота. без родителей',
+      translate: `orphaned`, 
+      url: orphaned
+    },
+    {
+      word: 'Поразительный, удивительный',
+      translate: `astounding`, 
+      url: astounding
+    },
+    {
+      word: 'Смотритель, сторож',
+      translate: `Custodian`, 
+      url: custodian
+    },
+    {
+      word: 'Сопровождающий',
+      translate: `chaperone`, 
+      url: chaperone
+    },
+    {
+      word: 'Нервный, испуганный',
+      translate: `jumpy`, 
+      url: jumpy
+    },
+    {
+      word: 'Горе, скорбь',
+      translate: `Grief`, 
+      url: grief
+    },
+    {
+      word: 'Злость, злоба',
+      translate: `Spite`, 
+      url: spite
+    },
+    {
+      word: 'Злоба, злонамеренность',
+      translate: `Malice`, 
+      url: malice
+    },
+    {
+      word: 'Сдаваться, капитулировать',
+      translate: `surrender`, 
+      url: surrender
+    },
+    {
+      word: 'Отрекаться, отказываться',
+      translate: `renounce`, 
+      url: renounce
+    },
+    {
+      word: 'Указ ,постановление',
+      translate: `Decree`, 
+      url: decree
+    },
+    {
+      word: 'Развод, расторжение брака',
+      translate: `Divorce`, 
+      url: divorce
+    },
+    {
+      word: 'Стрельба из лука, арбалета',
+      translate: `archery`, 
+      url: archery
+    },
+    {
+      word: 'Яд, ядовитый',
+      translate: `Poison`, 
+      url: poison
+    },
     
     
     
--- src/units/unit6.js
+++ src/units/unit6.js
@@ -1,23 +1,38 @@
-import remain from '../img/remain.jpg';
 import iamdiggingwithmusic from '../img/iamdiggingwithmusic.jpg';
 import pissedOffAbout from '../img/pissedOffAbout.jpg';
-import giveAHand from '../img/giveAHand.jpg';
 import beAlwaysThereForMe from '../img/beAlwaysThereForMe.jpg';
 import obviously from '../img/obviously.jpg';
-import prove from '../img/prove.jpg';
-import necessary from '../img/necessary.jpg';
 import fear from '../img/fear.jpg';
 import realize from '../img/realize.jpg';
+import thumb from '../img/thumb.jpg';
+import rate from '../img/rate.jpg';
+import rated from '../img/rated.jpg';
+import contented from '../img/contented.jpg';
+import timetable from '../img/timetable.jpg';
+import forInstance from '../img/forInstance.jpg';
+import recoverFrom from '../img/recoverFrom.jpg';
+import recover from '../img/recover.jpg';
+import weekendOfClubbing from '../img/weekendOfClubbing.jpg';
+import passionately from '../img/passionately.jpg';
+import comprehension from '../img/comprehension.jpg';
+import participateIn from '../img/participateIn.jpg';
+import haveASayIn from '../img/haveASayIn.jpg';
+import rise from '../img/rise.jpg';
+import riseUp from '../img/riseUp.jpg';
+import established from '../img/established.jpg';
+import spreadsheet from '../img/spreadsheet.jpg';
+import justify from '../img/justify.jpg';
+import expand from '../img/expand.jpg';
+import contribute from '../img/contribute.jpg';
+import overtake from '../img/overtake.jpg';
+import stareAt from '../img/stareAt.jpg';
+import generosity from '../img/generosity.jpg';
+import assignments from '../img/assignments.jpg';
 
 
 
 const unit6 = [
     
-    {
-      word: 'Оставаться (пребывать где-то)',
-      translate: 'remain', 
-      url: remain
-    },
     {
       word: 'Я кайфую от музыки',
       translate: "i'm digging with music", 
@@ -27,11 +42,6 @@ const unit6 = [
       word: 'Возмущен (разозлился из-за)',
       translate: "pissed off about", 
       url: pissedOffAbout
-    },
-    {
-      word: 'Помочь, подать руку помощи',
-      translate: "give a hand", 
-      url: giveAHand
     },
     {
       word: 'Всегда готов помочь мне',
@@ -44,16 +54,6 @@ const unit6 = [
       url: obviously
     },
     {
-      word: 'Доказывать',
-      translate: "prove", 
-      url: prove
-    },
-    {
-      word: 'Важный, необходимый',
-      translate: "necessary", 
-      url: necessary
-    },
-    {
       word: 'Страх (бояться)',
       translate: "Fear", 
       url: fear
@@ -63,9 +63,131 @@ const unit6 = [
       translate: "realize", 
       url: realize
     },
+    {
+      word: 'Большой палец',
+      translate: "Thumb", 
+      url: thumb
+    },
+    {
+      word: 'Оценивать, показатель',
+      translate: "rate", 
+      url: rate
+    },
+    {
+      word: 'Оцененный, считающийся',
+      translate: "rated", 
+      url: rated
+    },
+    {
+      word: 'Довольный, удовлетворенный, счастливый',
+      translate: "contented", 
+      url: contented
+    },
+    {
+      word: 'График, расписание, сроки',
+      translate: "timetable", 
+      url: timetable
+    },
+    {
+      word: 'Например',
+      translate: "for instance", 
+      url: forInstance
+    },
+    {
+      word: 'Избавиться от, излечиться от',
+      translate: "recover from", 
+      url: recoverFrom
+    },
+    {
+      word: 'Восстановить',
+      translate: "recover", 
+      url: recover
+    },
+    {
+      word: 'Выхрдные по клубам',
+      translate: "weekend of clubbing", 
+      url: weekendOfClubbing
+    },
+    {
+      word: 'Страстно',
+      translate: "passionately", 
+      url: passionately
+    },
+    {
+      word: 'Понимание, восприятие чего-то',
+      translate: "Comprehension", 
+      url: comprehension
+    },
+    {
+      word: 'Учавствовать в, принимать участие в',
+      translate: "participate in", 
+      url: participateIn
+    },
+    {
+      word: 'Иметь право голоса в ...',
+      translate: "have a say in", 
+      url: haveASayIn
+    },
+    {
+      word: 'Рост, подъем, увеличение',
+      translate: "rise", 
+      url: rise
+    },
+    {
+      word: 'Подняться, встать, восстать',
+      translate: "rise up", 
+      url: riseUp
+    },
+    {
+      word: 'Установленный, созданный',
+      translate: "established", 
+      url: established
+    },
+    {
+      word: 'Таблица, электронная таблица',
+      translate: "Spreadsheet", 
+      url: spreadsheet
+    },
+    {
+      word: 'Оправдать, доказать, подтвердить, обосновать',
+      translate: "justify", 
+      url: justify
+    },
+    {
+      word: 'Расширить, развить, увеличить',
+      translate: "expand", 
+      url: expand
+    },
+    {
+      word: 'Помочь, посодействовать',
+      translate: "contribute", 
+      url: contribute
+    },
+    {
+      word: 'Обгонять',
+      translate: "overtake", 
+      url: overtake
+    },
+    {
+      word: 'Пялиться на ... ',
+      translate: "stare at", 
+      url: stareAt
+    },
+    {
+      word: 'Великодушие, доброта, щедрость',
+      translate: "Generosity", 
+      url: generosity
+    },
+    {
+      word: 'Поручение, задача',
+      translate: "Assignment", 
+      url: assignments
+    },
+
 
   ]
 
 export default unit6;
+
 
 
